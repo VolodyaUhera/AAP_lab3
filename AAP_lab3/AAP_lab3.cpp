c98c4c662e1cb2402a92435d535442416d67d3c3
Voha <ugeravoloda@gmail.com>
2022-10-21 18:14:58 +0300

    task2 almost over

--- AAP_lab3/AAP_lab3.cpp
+++ AAP_lab3/AAP_lab3.cpp
@@ -1,27 +1,40 @@
 #include <iostream>
-
 #include <ctime>
 #include <chrono>
 
+
 //create 2d array
-#define n 3 //size array
-int c[n][n]{}; // array
+#define n 6 
+int c[n][n]{}; // array for task1
+
+#define h 3
+#define l 3
+int a[h][l]{}; // array for task2
 
 using namespace std;
+
 using namespace std::chrono;
 
-void GenerateElements()
+void headertask1() 
+{
+    printf("Лабораторна робота №3");
+    printf("Завдання 1. Програма для знаходження c[i][j] = c[j][i] ");
+    printf("та знайти їх кількість");
+}
+
+void GenerateElementsTask1()
 {
     srand(time(0));
     static float min = -10, max = 30.9;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            cout << "i = " << i << "j = " << j << endl;
+            //cout << "i = " << i << "j = " << j << endl;
             c[i][j] = (int)(min + (rand() % (int)(max - min + 1)));
         }
     }
 }
-void DisplayElements()
+
+void DisplayElementsTask1()
 {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
@@ -30,6 +43,7 @@ void DisplayElements()
         cout << '\n';
     }
 }
+
 void FindSameElementsAndShowIt()
 {
     int count = 0;
@@ -48,8 +62,8 @@ void FindSameElementsAndShowIt()
                     //cout << "f = " << f << '\n' << endl;
                     if (temp == c[k][f] && (x + y) != (k + f)) {
                         count++;
-                        cout << "c[y][x] = " << y << ' ' << x << " c[k][f] = " << k << ' ' << f  << endl;
-                        printf("%d \n", c[x][y]);
+                        cout << "c[y][x] = " << y << ' ' << x << " c[k][f] = " << k << ' ' << f  << "\t"<< endl;
+                        printf("%d  \n",  c[k][f]);
                         break;
                     }
                 }
@@ -62,8 +76,9 @@ void FindSameElementsAndShowIt()
 
 void task1() {
     auto start = high_resolution_clock::now();
-    GenerateElements();
-    DisplayElements();
+    headertask1();
+    GenerateElementsTask1();
+    DisplayElementsTask1();
     FindSameElementsAndShowIt();
     auto stop = high_resolution_clock::now();
     auto duration = duration_cast<microseconds>(stop - start);
@@ -71,8 +86,82 @@ void task1() {
     
 }
 
+void headertask2()
+{
+    printf("Лабораторна робота №3");
+    printf("Завдання 2. Вивести номері стовпців де всі елементи додатні");
+    printf("та рядки де всі елементи відємні");
+}
+
+void GenerateElementsTask2()
+{
+    srand(time(0));
+    static float min = -10, max = 30.9;
+    for (int i = 0; i < h; i++) {
+        for (int j = 0; j < l; j++) {
+            //cout << "i = " << i << "j = " << j << endl;
+            a[i][j] = (int)(min + (rand() % (int)(max - min + 1)));
+        }
+    }
+}
+
+void DisplayElementsTask2()
+{
+    {
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < l; j++) {
+                cout << a[i][j] << '\t';
+            }
+            cout << '\n';
+        }
+    }
+
+}
+
+bool is_low_then_zero(int i)
+{
+    return (i < 0);
+}
+void FindSameElementsInRows()
+{
+
+    int count{};
+    int row{};
+    int sum{};
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < l; j++)
+        {
+            if (a[i][j] < 0)
+            {
+                count++;
+                if (count == 5)
+                {
+                    cout << count << endl;
+                    sum++;
+                    break;
+
+                }
+            }
+        }
+    }
+}
+
+void task2()
+{
+    auto start = high_resolution_clock::now();
+    headertask2();
+    GenerateElementsTask2();
+    DisplayElementsTask2();
+    FindSameElementsInRows();
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "duration taken (milliseconds) = " << duration.count() / 1000 << endl;
+}
+
 int main() {
     setlocale(LC_ALL, "Ukrainian");
-    task1();
+    //task1();
+    task2();
 
 }
